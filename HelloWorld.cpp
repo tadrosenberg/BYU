55f52bd9b3f10dffb9611257b766f1b8f95b028b
Tad Rosenberg <tadrosenberg@diane.local>
2022-09-01 10:25:55 -0600

    Hello World Program

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // allows you to print streams to the command line
+
+using namespace std; //
+
+int main() {
+
+    cout << "hello world" << endl;
+    cout << "i cant believe im really speaking" << endl;
+    return 0;
+
+}
